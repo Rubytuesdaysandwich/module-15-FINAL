672430b9e6481e74576ce90e7b20810d4c481c09
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-07-21 10:28:57 -0600

    instructions

--- php_final/public/editGrave.php
+++ php_final/public/editGrave.php
@@ -1,7 +1,7 @@
 <?php require_once("../includes/db_connection.php"); ?>
 <?php require_once("../includes/session.php"); ?>
 <?php require_once("../includes/functions.php"); ?>
-<?php confirm_logged_in(); ?>
+<?php confirm_logged_in();//confirm user credentials and they are logged in. ?>
 
 <?php
 
